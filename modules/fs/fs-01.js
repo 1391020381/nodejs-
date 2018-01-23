fbf55edc04318ac4aec9404e8dea4ab14c87561e
zhoujin <1391020381@qq.com>
2018-01-23 16:48:23 +0800

    fs-stats

--- modules/fs/fs-01.js
+++ modules/fs/fs-01.js
@@ -9,3 +9,47 @@ fs.readFile(path.join(_dirname, '/static/1.txt'), (err, data) => {
 
 let text = fs.readFileSync(path.join(_dirname, '/static/1.txt'), 'utf-8')
 console.log('text:', text)
+
+fs.exists(path.join(_dirname, '/static/1.txt'), (exists) => {
+  console.log('exists:', exists)
+})
+
+fs.readdir(process.cwd(), (err, files) => {  // 用于读取目录,返回一个所包含的文件和子目录的数组
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('files:', files)
+})
+
+// stat
+fs.readdir(path.join(_dirname, '/modules'), (err, files) => {
+  // console.log('stat:', files)    // [ 'buffer', 'commonJS', 'debug.js', 'events', 'fs', 'path', 'stream' ]
+  let tempFile = [], tempDir = [], tempStats = []
+  files.forEach((file, index) => {
+    // fs.stat(path.join(_dirname, '/modules', file), (err, stats) => {
+    //     if (err) throw err
+    //     console.log('isFile:', stats.isFile(), 'isDirectory:', stats.isDirectory())
+    //     if (stats.isFile()) {
+    //       tempFile.push(file)
+    //     } else if (stats.isDirectory()) {
+    //       tempDir.push(file)
+    //     }
+    //     // console.log('stats:', stats)
+    //   }
+    // )
+    tempStats.push({stats: fs.statSync(path.join(_dirname, '/modules', file)), index: index})
+  })
+  // console.log('tempStsts:', tempStats)
+  tempStats.forEach(i => {
+    if (i.stats.isFile()) {
+      tempFile.push(files[i.index])
+    } else if (i.stats.isDirectory()) {
+      tempDir.push(files[i.index])
+    }
+  })
+
+  console.log('tempFile:', tempFile, 'tempDir:', tempDir)
+})
+
+
--- modules/fs/fs.md
+++ modules/fs/fs.md
@@ -9,4 +9,7 @@
 1. readFile(),readFileSync()
 2. writeFile(),writeFileSync()
 3. exists(path,callback)<判断给定的路径是否存在,然后不管结果如何,都会调用回调函数。不要在open方法之前调用exists方法,open方法本身就能检测文件是否存在>
-
+4. mkdir(),writeFile(),readFile()
+5. mkdirSync(),writeFileSynce(),readFileSync()
+6. readdir(),readdirSync()  <用于读取目录,返回一个所包含的文件和子目录的数组>
+7. stat() stat方法的参数是一个文件或者目录,它产生一个对象,该对象包含了该文件或者目录的具体信息。我们往往通过该方法,判断正在处理的到底是一个文件还是目录。
